913e9665de36c7c3bd2b4625154a8063903e5f09
Pap Kristóf <pap.kristof.199411@gmail.com>
2017-07-04 23:05:10 +0200

    Játékosok osztálya

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,18 @@
 package main;
 
+import modell.Jatekosok;
+import modell.Kartyak;
+
 public class Main {
 
     public static void main(String[] args){
 
+        Jatekosok jatekos1 = new Jatekosok("Player 1");
+        Jatekosok jatekos2 = new Jatekosok("Player 2");
         Kartyak kartyak = new Kartyak();
-        System.out.println(kartyak.generator());
+
+        System.out.println(jatekos1.getName() + ": " + kartyak.generator() + " " + kartyak.generator());
+        System.out.println(jatekos2.getName() + ": " + kartyak.generator() + " " + kartyak.generator());
 
     }
 
---
+++ src/modell/Jatekosok.java
@@ -0,0 +1,19 @@
+package modell;
+
+public class Jatekosok {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Jatekosok(String name) {
+        this.name = name;
+    }
+
+}
