950d36778b40e704d875cfa7e04c23bc772737b8
Pap Kristóf <pap.kristof.199411@gmail.com>
2017-07-04 22:33:53 +0200

    First commit...

---
+++ src/main/Kartyak.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Kartyak {
+
+    public int generator(){
+
+        int[] kartyak = {11,10,9,8,7,4,3,2};
+        int random = (int) (Math.random() * kartyak.length);
+
+        Integer kartya = kartyak[random];
+
+        return kartya;
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Kartyak kartyak = new Kartyak();
+        System.out.println(kartyak.generator());
+
+    }
+
+}
